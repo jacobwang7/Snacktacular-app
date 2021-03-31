f715445a5d2542207e64ba3a5e340619660e44b1
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-03-31 13:00:23 -0400

    .gitignore

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Jacob Wang on 3/31/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCicCpYAvD8s13z_gydRKXAwPMgD3erCfg"
+}
